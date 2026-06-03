e4eeaf6f9a10ac3e19c2842ae435f4d9c7c837d2
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-03 08:38:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let numeros = [1, 2, 3, 4, 5];
+let soma = 0;
+
+for (let i = 0; i < 5; i++) {
+    numeros[i] = Number(prompt("Digite um número:"));
+    soma += numeros[i];
+}
+
+let media = soma / 5;
+
+alert("Média = " + media);
