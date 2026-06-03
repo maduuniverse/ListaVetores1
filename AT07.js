534ddabdf4770566c921c8e965b017e1a7bc98b9
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-03 08:44:57 -0300

    Update AT07.js

--- AT07.js
+++ AT07.js
@@ -1,12 +1,15 @@
 let numeros = [1, 2, 3, 4, 5];
-let pares = 0;
 
 for (let i = 0; i < 5; i++) {
     numeros[i] = Number(prompt("Digite um número:"));
+}
 
-    if (numeros[i] % 2 === 0) {
-        pares++;
+let maior = numeros[0];
+
+for (let i = 1; i < 5; i++) {
+    if (numeros[i] > maior) {
+        maior = numeros[i];
     }
 }
 
-alert("Quantidade de pares: " + pares);
+alert("Maior número:", maior);
